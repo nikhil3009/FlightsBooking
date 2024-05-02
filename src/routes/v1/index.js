b426d6c55d42c9b52268b73ec334edbca19c5b48
nikhil3009 <sainikhil3009@gmail.com>
2024-05-02 13:38:14 -0400

    Error Handling

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -5,6 +5,5 @@ const airplaneRoutes = require('./airplane-routes');
 const router = express.Router();
 console.log('inside v1 routes');
 router.use('/airplanes', airplaneRoutes);
-
 router.get('/info', InfoController.info);
 module.exports = router;
