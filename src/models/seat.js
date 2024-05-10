877a473ae55bcc7d88650864734ffac656e6338b
nikhil3009 <sainikhil3009@gmail.com>
2024-05-10 14:24:40 -0400

    Finished Flights Service

--- src/models/seat.js
+++ src/models/seat.js
@@ -11,7 +11,11 @@ module.exports = (sequelize, DataTypes) => {
 		 * This method is not a part of Sequelize lifecycle.
 		 * The `models/index` file will call this method automatically.
 		 */
-		static associate(models) {}
+		static associate(models) {
+			this.belongsTo(models.Airplane, {
+				foreignKey: 'airplaneId',
+			});
+		}
 	}
 	Seat.init(
 		{
