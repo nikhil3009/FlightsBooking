c29a88f822638dcfc32cad0702b58c071f0a202d
Sai Nikhil Reddy <sainikhil3009@gmail.com>
2024-06-15 22:24:52 -0400

    Service Completion

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 /** @format */
 
 const express = require('express');
-
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
-
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
